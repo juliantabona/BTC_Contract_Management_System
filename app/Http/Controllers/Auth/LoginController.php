31119e21f41d5411945e1ae5ac875eb790b41362
Julian <brandontabona@gmail.com>
2018-07-14 12:45:51 +0200

    Allow login via email/username, change login view, and add image
    We updated the LoginController to allow both authentication using email/username. We also update the login view to be able to accept both email/username under the same input called "identity". We change the directory names for where the application images and logos are placed. Uploaded image for login page and update the style.css to use that image. Added the BTC Logo to login screen.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,6 +21,28 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     /**
+     * Allow for login using email and username.
+     */
+    public function username()
+    {
+        //  Grab the users input for email/usermane
+        $identity = request()->input('identity');
+
+        /*
+            Determine if the field provided is a valid email
+            If it is then we know to return this as a login with email
+            Otherwise we assume that this is an intended login using the username
+        */
+        $field = filter_var($identity, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        //  Update the request to know whether to try login using the email/username
+        request()->merge([$field => $identity]);
+
+        //  Continue with login attempt
+        return $field;
+    }
+
+    /**
      * Where to redirect users after login.
      *
      * @var string
@@ -29,8 +51,6 @@ class LoginController extends Controller
 
     /**
      * Create a new controller instance.
-     *
-     * @return void
      */
     public function __construct()
     {
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,66 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+    <!-- content-wrapper starts -->
+    <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-full-bg">
+        <div class="row w-100">
+            <div class="col-lg-4 mx-auto p-0">
+                <div class="auth-form-dark text-left p-5">
+                    <img src="/assets/logos/btc-logo-260x145.png" alt="logo" class="d-block m-auto" style="width: 120px;">
+                    <h4 class="font-weight-light mt-4 text-center">Login</h4>
+                    <form method="POST" action="{{ route('login') }}" class="pt-4">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="form-group
+                                    {{ $errors->has('username') ? ' has-error' : '' }}
+                                    {{ $errors->has('email') ? ' has-error' : '' }}
+                                    ">
+                            <input id="identity" type="text" class="form-control 
+                                                {{ $errors->has('username') ? ' is-invali d' : '' }}
+                                                {{ $errors->has('email') ? ' is-invalid' : '' }}" 
+                                                name="identity" value="{{ old('identity') }}"
+                                placeholder="Email/Username" required autofocus>
+                            <i class="mdi mdi-account"></i>
+                            @if ($errors->has('username'))
+                            <span class="help-block invalid-feedback d-block">
+                                <strong>Username & Password don't match</strong>
+                            </span>
+                            @endif
+                            @if ($errors->has('email'))
+                            <span class="help-block invalid-feedback d-block">
+                                <strong>Email & Password don't match</strong>
+                            </span>
+                            @endif
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <input type="password" class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
+                            <i class="mdi mdi-eye"></i>
+                            @if ($errors->has('password'))
+                            <span class="help-block invalid-feedback d-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                            @endif
                         </div>
-
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" {{ old( 'remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
                                     </label>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
+                        <div class="mt-5">
+                            <button type="submit" class="btn btn-block btn-warning btn-lg font-weight-medium">
+                                Login
+                            </button>
+                        </div>
+                        <div class="mt-3 text-center">
+                            <a href="{{ route('password.request') }}" class="auth-link text-white">Forgot password?</a>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <!-- content-wrapper ends -->
 @endsection
